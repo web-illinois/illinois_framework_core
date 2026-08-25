4ba92ab16824ab7af70bd1a026db0975d4ee11e6
Tyler Jackson <150186522+tjackson2113@users.noreply.github.com>
2026-08-25 09:20:29 -0500

    removed post update

--- modules/illinois_framework_core_trash_admin/illinois_framework_core_trash_admin.post_update.php
+++
@@ -1,8 +0,0 @@
-<?php
-
-/**
- * Synchronize Trash permissions with the Content Manager role.
- */
-function illinois_framework_core_trash_admin_post_update_sync_trash_permissions(&$sandbox) {
-  _illinois_framework_core_trash_admin_sync_role();
-}
